34249e911b780964550dd18d71da66a12aa6ca7f
Mubeen Mehmood <mubeendev3@gmail.com>
2024-05-01 04:43:31 -0700

    UI Fixes

--- app/page.tsx
+++ app/page.tsx
@@ -4,18 +4,25 @@ import Link from "next/link";
 const HomePage = () => {
   return (
     <div className="flex h-screen">
-      <div className="content-section w-[55%] bg-customBlue">
-        <div className="p-60 flex flex-col gap-5 items-start">
-          <p className="text-yellow-300 text-3xl">Hello, I'm</p>
-          <h1 className="text-white text-[100px] font-bold leading-[95px]">
-            Mubeen Mehmood
-          </h1>
-          <p className="text-gray-400 text-4xl font-light">Web Developer</p>
-          <button className="text-black bg-customYellow py-2 px-6 rounded-full">
-            <Link href="https://github.com/mubeendev3" target="_blank">
-              Checkout My Github&apos;
-            </Link>
-          </button>
+      <div className="content-section w-[55%] bg-customBlue ">
+        <div className="flex flex-col gap-5 items-center justify-center h-screen">
+          <div>
+            <p className="text-yellow-300 text-3xl pb-4">Hello, I'm</p>
+            <h1 className="text-white text-[100px] font-bold leading-[95px] pb-4">
+              Mubeen
+            </h1>
+            <h1 className="text-white text-[100px] font-bold leading-[95px] pb-4">
+              Mehmood
+            </h1>
+            <p className="text-gray-400 text-4xl font-light pb-4">
+              Web Developer
+            </p>
+            <button className="text-black bg-customYellow py-2 px-6 rounded-full mt-5">
+              <Link href="https://github.com/mubeendev3" target="_blank">
+                Checkout My Github&apos;
+              </Link>
+            </button>
+          </div>
         </div>
       </div>
       <div className="image-section w-[45%] bg-customBlue flex items-center justify-center">
